1a70f5dd10117cb1ca9c073d3f0010b093b50265
iey704 <iey704@gachon.ac.kr>
2024-04-02 17:08:12 +0900

    Feat:사이드바 수정

---
+++ material-kit-react/src/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'Nam Somi',
+  email: 'namsomi@gachon.ac.kr',
+  photoURL: '/assets/images/avatars/somi.jpg',
+};
