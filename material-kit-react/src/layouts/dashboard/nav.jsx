ac0e5ea2c0cddc13c2cb2f7ef8dfb310ce5b2e02
iey704 <iey704@gachon.ac.kr>
2024-04-05 15:47:56 +0900

    Feat: 로고

--- material-kit-react/src/layouts/dashboard/nav.jsx
+++ material-kit-react/src/layouts/dashboard/nav.jsx
@@ -80,7 +80,7 @@ export default function Nav({ openNav, onCloseNav }) {
         },
       }}
     >
-      <Logo sx={{ mt: 3, ml: 4 }} />
+      <Logo sx={{ mt: 3, ml: 2 }} />
 
       {renderAccount}
 
