406b796af8709505c1091838aad6fe8866258bd7
iey704 <iey704@gachon.ac.kr>
2024-04-17 20:12:45 +0900

    fix: component name

--- src/layouts/dashboard/common/account-modal.jsx
+++ src/layouts/dashboard/common/account-modal.jsx
@@ -15,7 +15,7 @@ import {
 } from '@mui/material';
 import Iconify from 'src/components/iconify';
 
-export default function AccountModal({ rightButton, mode, onClose, open, title, contents }) {
+export default function AccountModal({ rightButton, mode, onClose, open }) {
   const [textField, setTextField] = useState('');
 
   const modal_style = {
@@ -80,33 +80,19 @@ export default function AccountModal({ rightButton, mode, onClose, open, title, 
                 // backgroundColor: 'pink',
               }}
             >
-              {mode !== 'content' && (<Typography id="modal-modal-title" variant="h4" component="h4">
-                {title}
-              </Typography>)}
-              {mode === 'textfield' && (
-                <>
-                  <Typography
-                    id="modal-modal-description"
-                    variant="body2"
-                    component="body2"
-                    color="text.secondary"
-                    sx={{ fontSize: '16px' }}
-                  >
-                    {contents}
-                  </Typography>
-                  <TextField
-                    id="filter-text"
-                    label="#"
-                    value={textField}
-                    onChange={(e) => setTextField(e.target.value)}
-                    margin="normal"
-                    sx={{
-                      width: 330,
-                      height: 53,
-                    }}
-                  />
-                </>
-              )}
+              <Typography id="modal-modal-title" variant="h4" component="h4">
+                Withdraw
+              </Typography>
+              <Typography
+                id="modal-modal-description"
+                variant="body2"
+                component="body2"
+                color="text.secondary"
+                sx={{ fontSize: '16px' }}
+              >
+                정말로 탈퇴하시겠습니까?
+              </Typography>
+
               {(mode === 'title' || mode === 'content') && (
                 <div
                   style={{
