7d12e09a290740a86d527d1551ff377cf1226560
iey704 <iey704@gachon.ac.kr>
2024-04-30 01:14:00 +0900

    feat: 토큰 받기 완성

--- src/sections/login/login-kakao-auth.jsx
+++ src/sections/login/login-kakao-auth.jsx
@@ -2,35 +2,26 @@ import axios from 'axios';
 import { useEffect } from 'react';
 
 export default function LoginKakaoAuth() {
+
+  useEffect(() => {
+
+    const client_id =import.meta.env.VITE_APP_K_REST_API_KEY; 
+    const K_REDIRECT_URI = 'http://localhost:3000/login-auth';
+    const grant_type = 'authorization_code';
+    // 인가코드 받기
+    const code = new URLSearchParams(window.location.search).get('code');
     
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                // 인가코드 받기
-                const code = new URLSearchParams(window.location.search).get('code');
-                console.log('인가코드:', code);
-
-                const headers = {
-                    'Content-Type':  'application/x-www-form-urlencoded',
-                };
-
-                const response = await axios.post(
-                    `http://localhost:8080/api/auth/login`,
-                    { code }, // 요청 본문에 code를 객체로 포함하여 전송
-                    { headers }
-                  );
-        
-                console.log(response.data);
-                console.log(response.data.result.user_id);
-                console.log(response.data.result.jwt);
-            } catch (e) {
-                console.error(e);
-            }
-            };
-        
-            fetchData();
+    console.log('인가코드:', code);
+    axios.post(`https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${K_REDIRECT_URI}&code=${code}`, 
+      { 
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        } 
+      }).then((res) => {
+        console.log(res);
+      })
     }, [])
-    
+
   return (
     <div>
       <h1>LoginAuth</h1>
